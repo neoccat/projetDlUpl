553e7776c702c9309912529847879a033f04c3d9
Theo Taccoen <theo.taccoen@cgi.com>
2023-01-03 14:27:48 +0100

    Fix Alexandre bebou

--- src/main/java/model/Upload.java
+++ src/main/java/model/Upload.java
@@ -1,36 +1,49 @@
 package model;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.oreilly.servlet.MultipartRequest;
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
 
 @WebServlet("/Upload")
-@MultipartConfig(
-  fileSizeThreshold = 1024 * 1024 * 1, // 1 MB
-  maxFileSize = 1024 * 1024 * 10,      // 10 MB
-  maxRequestSize = 1024 * 1024 * 100   // 100 MB
-)
 public class Upload extends HttpServlet {
+    private final int maxFileSize = 1024*1024*100000;
 
-	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		try {
-			MultipartRequest m = new MultipartRequest(req, "src/main/webapp/users_files/" + req.getSession().getAttribute("user")); 
-			
-			RequestDispatcher rd = req.getRequestDispatcher("files.jsp");
-			rd.forward(req, resp);
-			
-		} catch(Exception e) {
-			RequestDispatcher rd = req.getRequestDispatcher("files.jsp?error=error");
-			rd.forward(req, resp);
-		}
-	}
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try {
+            //MultipartRequest m = new MultipartRequest(req, "src/main/webapp/users_files/" + req.getSession().getAttribute("user")); 
+            DiskFileItemFactory factory = new DiskFileItemFactory(maxFileSize, new File("src/main/webapp/users_files/" + req.getSession().getAttribute("user")));
+            
+            ServletFileUpload upload = new ServletFileUpload(factory);
+
+            // Set overall request size constraint
+            upload.setSizeMax(maxFileSize);
+
+            // Parse the request
+            List<FileItem> items = upload.parseRequest(req);
+                        
+            for(FileItem fi : items) {
+                File uploadedFile = new File("src/main/webapp/users_files/" + req.getSession().getAttribute("user") +"/"+fi.getName());
+                fi.write(uploadedFile);
+            }
+            
+            RequestDispatcher rd = req.getRequestDispatcher("files.jsp");
+            rd.forward(req, resp);
+            
+        } catch(Exception e) {
+            RequestDispatcher rd = req.getRequestDispatcher("files.jsp?error=error");
+            rd.forward(req, resp);
+        }
+    }
 }
